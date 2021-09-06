ef1c05f4276ca10471f9e1f2fde6fe8bb2cb10cd
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-09-07 00:43:45 +0300

    docs: moved documentation for the crate to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,4 @@
-//! This crate provides types and functions for abstracting MIDI files.
-//!
-//! This crate works alongside with the [midly][] and [midir][] crates.
-//!
-//! # A Basic Workflow
-//!
-//! - Use [midly][] to parse a MIDI file.
-//! - Create a [Ticker] from the `header`.
-//! - Create a [Sheet] from the parsed [tracks](midly::Track).
-//! - Initialize a MIDI connection using [midir][].
-//! - Create a [Player] from the connection and the timer.
-//! - Play the sheet using the player.
-//!
-//! # Examples
-//!
-//! Please check out the source code of [plmidi][] for an implementation.
-//!
-//! [midir]: https://crates.io/crates/midir
-//! [midly]: https://crates.io/crates/midly
-//! [plmidi]: https://github.com/insomnimus/plmidi
+#![doc = include_str!("doc_lib.md")]
 
 mod event;
 mod player;

b9cecee7cb99de4077533ceb786e12b28bf05698
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-09-06 22:44:36 +0300

    docs: documented the Sheet module

--- readme.md
+++ readme.md
@@ -1,43 +1,4 @@
-# plmidi
+# Nodi
 
-A library and a command for playing MIDI files.
+Nodi is a library for time-mapping and playing of MIDI files.
 
-# Installation
-
-```shell
-# to install after a git clone
-git clone https://github.com/insomnimus/plmidi
-cd plmidi
-git checkout main
-cargo install --path . --locked
-
-# here's a one liner:
-# cargo install --locked --branch main --git https://github.com/insomnimus/plmidi
-```
-
-# Command Usage
-
-```òutput
-plmidi 0.1.2
-
-Taylan G├╢kkaya <insomnimus.dev@gmail.com>
-
-Play MIDI files.
-
-USAGE:
-    plmidi.exe [OPTIONS] [file]
-
-ARGS:
-    <file>    A MIDI file (.mid) to be played.
-
-OPTIONS:
-    -d, --device <device>    The index of the MIDI device that will be used for synthesis. [default:
-                             0]
-    -h, --help               Print help information
-    -l, --list               List available MIDI output devices.
-    -V, --version            Print version information
-```
-
-# TODO
-
-Library usage and documentation coming soon.
--- src/sheet.rs
+++ src/sheet.rs
@@ -14,18 +14,11 @@ use crate::event::{
 	Moment,
 };
 
-#[derive(Default, Clone, Debug)]
+/// Holds every moment in a MIDI track, each moment representing a MIDI tick.
+///
+/// This type is used for time-mapping a MIDI track.
+#[derive(Default, Clone, Debug, PartialEq)]
 pub struct Sheet(pub(crate) Vec<Moment>);
-
-impl Sheet {
-	pub fn len(&self) -> usize {
-		self.0.len()
-	}
-
-	pub fn is_empty(&self) -> bool {
-		self.0.is_empty()
-	}
-}
 
 impl Index<usize> for Sheet {
 	type Output = Moment;
@@ -72,14 +65,88 @@ impl<'a> From<&[TrackEvent<'a>]> for Sheet {
 }
 
 impl Sheet {
-	pub fn from_track_events(events: &[TrackEvent<'_>]) -> Self {
+	/// Creates a [Sheet] from a slice of [TrackEvent]s.
+	///
+	/// # Remarks
+	/// Use this when the MIDI file header specifies the format to be 0, meaning
+	/// `single`.
+	pub fn single(events: &[TrackEvent<'_>]) -> Self {
 		events.into()
 	}
 
+	/// Creates a [Sheet] from many tracks, merging all of them into one.
+	///
+	/// # Remarks
+	/// Use this when a MIDI file header specifies the format to be of 1,
+	/// meaning parallel.
+	pub fn parallel(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
+		if tracks.is_empty() {
+			return Self::default();
+		}
+
+		let mut first = Self::from(tracks[0].as_slice());
+
+		for track in &tracks[1..] {
+			let sh = Self::from(track.as_slice());
+			first.merge_with(sh);
+		}
+		first
+	}
+
+	/// Creates a [Sheet] from every track, appending them end to
+	/// end.
+	///
+	/// # Remarks
+	/// Use this when a MIDI file header specifies the type as 2, meaning
+	/// `sequential`.
+	pub fn sequential(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
+		if tracks.is_empty() {
+			return Self::default();
+		}
+
+		let mut first = Self::from(tracks[0].as_slice());
+
+		for track in &tracks[1..] {
+			let sh = Self::from(track.as_slice());
+			first.append(sh);
+		}
+		first
+	}
+
+	/// Creates an instance of [Self] from a [Vec] of [Moments](Moment).
+	pub fn new(moments: Vec<Moment>) -> Self {
+		Self(moments)
+	}
+
+	/// Returns the wrapped `Vec<Moment>`, destroying `self`.
+	pub fn into_inner(self) -> Vec<Moment> {
+		self.0
+	}
+
+	/// Returns how many MIDI ticks (or [Moment]s) this [Sheet] has.
+	pub fn len(&self) -> usize {
+		self.0.len()
+	}
+
+	/// Returns `true` if `Self::len() == 0`.
+	pub fn is_empty(&self) -> bool {
+		self.0.is_empty()
+	}
+
+	/// Appends another [Sheet] to `self`, destroying the other.
+	///
+	/// # Remarks
+	/// This method will join two tracks end to end. If you want to merge them
+	/// instead, see [Sheet::merge_with].
 	pub fn append(&mut self, other: Self) {
 		self.0.extend(other.0);
 	}
 
+	/// Merges `self` with another [Sheet], destroying the other.
+	///
+	/// # Remarks
+	/// This method will combine every moment in both [Sheet]s into one. If you
+	/// want to join them end to end instead, see [Sheet::append].
 	pub fn merge_with(&mut self, other: Self) {
 		if other.len() > self.len() {
 			let n_new = other.len() - self.len();
@@ -93,19 +160,5 @@ impl Sheet {
 				}
 			}
 		}
-	}
-
-	pub fn parallel(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
-		if tracks.is_empty() {
-			return Self::default();
-		}
-
-		let mut first = Self::from(tracks[0].as_slice());
-
-		for track in &tracks[1..] {
-			let sh = Self::from(track.as_slice());
-			first.merge_with(sh);
-		}
-		first
 	}
 }

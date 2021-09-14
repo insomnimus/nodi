69116242bf1cef474d379eb526d9211fdc5efb9e
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-09-15 01:23:04 +0300

    perf: improved Sheet::into_bars performance

--- src/sheet/bar.rs
+++ src/sheet/bar.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 
 use crate::{Event, Moment, Sheet};
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 struct TimeSignature {
     // Beats per bar.
     numerator: u8,
@@ -20,7 +20,7 @@ impl TimeSignature {
 
 pub struct Bars {
     time_sig: TimeSignature,
-    beat_32s: u8,
+    // beat_32s: u8,
     tpb: f32,
     buf: VecDeque<Moment>,
 }
@@ -33,58 +33,27 @@ impl Iterator for Bars {
             return None;
         }
         // Check if start of bar has time signature.
-        if let Some((time_sig, beat_32s)) = self.buf.front().and_then(|m| match &m {
-            Moment::Empty => None,
-            Moment::Events(events) => events
-                .iter()
-                .flat_map(|e| match e {
-                    Event::TimeSignature(numerator, denominator, _, beat_32s) => Some((
-                        TimeSignature {
-                            numerator: *numerator,
-                            denominator: *denominator,
-                        },
-                        beat_32s,
-                    )),
-                    _ => None,
-                })
-                .next(),
-        }) {
+        let first = self.buf.pop_front().unwrap();
+        if let Some(time_sig) = find_time_sig(&first) {
             self.time_sig = time_sig;
-            self.beat_32s = *beat_32s;
+        }
+        if self.buf.is_empty() {
+            return Some(vec![first]);
         }
 
-        let len_32nd = self.tpb / self.beat_32s as f32;
+        let len_32nd = self.tpb / 8.0; // self.beat_32s as f32;
         let chunk_len = (self.time_sig.bar_32s() * len_32nd as f32) as usize;
         let mut temp = Vec::with_capacity(chunk_len);
+        temp.push(first);
 
-        for _ in 0..chunk_len {
+        for _ in 1..chunk_len {
             if let Some(moment) = self.buf.pop_front() {
-                match &moment {
-                    Moment::Empty => temp.push(moment),
-                    Moment::Events(events) => {
-                        if let Some((&numerator, &denominator, &beat_32s)) = events
-                            .iter()
-                            .flat_map(|e| match e {
-                                Event::TimeSignature(nom, denom, _, beat_32s) => {
-                                    Some((nom, denom, beat_32s))
-                                }
-                                _ => None,
-                            })
-                            .next()
-                        {
-                            let ts = TimeSignature {
-                                numerator,
-                                denominator,
-                            };
-                            temp.push(moment);
-                            if ts != self.time_sig || beat_32s != self.beat_32s {
-                                self.beat_32s = beat_32s;
-                                self.time_sig = ts;
-                                break;
-                            }
-                        } else {
-                            temp.push(moment);
-                        }
+                let time_sig = find_time_sig(&moment);
+                temp.push(moment);
+                if let Some(time_sig) = time_sig {
+                    if time_sig != self.time_sig {
+                        self.time_sig = time_sig;
+                        break;
                     }
                 }
             } else {
@@ -109,8 +78,25 @@ impl Sheet {
                 numerator: 4,
                 denominator: 4,
             },
-            beat_32s: 24,
+            // beat_32s: 24,
             buf: self.0.into(),
+        }
+    }
+}
+
+fn find_time_sig(m: &Moment) -> Option<TimeSignature> {
+    match m {
+        Moment::Empty => None,
+        Moment::Events(events) => {
+            for e in events {
+                if let Event::TimeSignature(n, d, ..) = e {
+                    return Some(TimeSignature {
+                        numerator: *n,
+                        denominator: *d,
+                    });
+                }
+            }
+            None
         }
     }
 }

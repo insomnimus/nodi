7981186d08068a42be21f50d10582b0e0a306b50
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-09-18 18:23:29 +0300

    feat: exported the `Bars` type and added some methods to `Sheet`

--- src/sheet.rs
+++ src/sheet.rs
@@ -5,116 +5,137 @@ use crate::event::Moment;
 mod bar;
 mod impls;
 
+pub use bar::Bars;
+
 #[doc = include_str!("doc_sheet.md")]
 #[derive(Default, Clone, Debug, PartialEq)]
 pub struct Sheet(pub(crate) Vec<Moment>);
 
 impl Sheet {
-    /// Creates a [Sheet] from a slice of [TrackEvent]s.
-    ///
-    /// # Remarks
-    /// Use this when the MIDI file header specifies the format to be 0, meaning
-    /// `single`.
-    pub fn single(events: &[TrackEvent<'_>]) -> Self {
-        events.into()
-    }
+	/// Creates a [Sheet] from a slice of [TrackEvent]s.
+	///
+	/// # Remarks
+	/// Use this when the MIDI file header specifies the format to be 0, meaning
+	/// `single`.
+	pub fn single(events: &[TrackEvent<'_>]) -> Self {
+		events.into()
+	}
 
-    /// Creates a [Sheet] from many tracks, merging all of them into one.
-    ///
-    /// # Remarks
-    /// Use this when a MIDI file header specifies the format to be of 1,
-    /// meaning parallel.
-    pub fn parallel(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
-        if tracks.is_empty() {
-            return Self::default();
-        }
+	/// Creates a [Sheet] from many tracks, merging all of them into one.
+	///
+	/// # Remarks
+	/// Use this when a MIDI file header specifies the format to be of 1,
+	/// meaning parallel.
+	pub fn parallel(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
+		if tracks.is_empty() {
+			return Self::default();
+		}
 
-        let mut first = Self::from(tracks[0].as_slice());
+		let mut first = Self::from(tracks[0].as_slice());
 
-        for track in &tracks[1..] {
-            let sh = Self::from(track.as_slice());
-            first.merge_with(sh);
-        }
-        first
-    }
+		for track in &tracks[1..] {
+			let sh = Self::from(track.as_slice());
+			first.merge_with(sh);
+		}
+		first
+	}
 
-    /// Creates a [Sheet] from every track, appending them end to
-    /// end.
-    ///
-    /// # Remarks
-    /// Use this when a MIDI file header specifies the type as 2, meaning
-    /// `sequential`.
-    pub fn sequential(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
-        if tracks.is_empty() {
-            return Self::default();
-        }
+	/// Creates a [Sheet] from every track, appending them end to
+	/// end.
+	///
+	/// # Remarks
+	/// Use this when a MIDI file header specifies the type as 2, meaning
+	/// `sequential`.
+	pub fn sequential(tracks: &[Vec<TrackEvent<'_>>]) -> Self {
+		if tracks.is_empty() {
+			return Self::default();
+		}
 
-        let mut first = Self::from(tracks[0].as_slice());
+		let mut first = Self::from(tracks[0].as_slice());
 
-        for track in &tracks[1..] {
-            let sh = Self::from(track.as_slice());
-            first.append(sh);
-        }
-        first
-    }
+		for track in &tracks[1..] {
+			let sh = Self::from(track.as_slice());
+			first.append(sh);
+		}
+		first
+	}
 
-    /// Creates an instance of [Self] from a [Vec] of [Moments](Moment).
-    pub fn new(moments: Vec<Moment>) -> Self {
-        Self(moments)
-    }
+	/// Creates a new, blank [Sheet].
+	pub fn new() -> Self {
+		Self(Vec::new())
+	}
 
-    /// Returns the wrapped `Vec<Moment>`, destroying `self`.
-    pub fn into_inner(self) -> Vec<Moment> {
-        self.0
-    }
+	/// Creates a new, blank [Sheet] with the given initial capacity.
+	pub fn with_capacity(cap: usize) -> Self {
+		Self(Vec::with_capacity(cap))
+	}
 
-    /// Returns how many MIDI ticks (or [Moment]s) this [Sheet] has.
-    ///
-    /// Note that multiplying this value with the length of a tick may not
-    /// always give you the correct total duration. The reason for this is a
-    /// MIDI file can change tempo mid-track, however it is still trivial to
-    /// calculate the duration since every tempo-change event will be contained
-    /// in `self`.
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
+	/// Creates a `Sheet` from the given [Vec].
+	///
+	/// Note: This function does not parse the given moments but instead just
+	/// wraps them with `Sheet`. Use [Sheet::single], [Sheet::parallel] or
+	/// [Sheet::sequential] if you want to calculate the time offsets.
+	pub fn with_moments(moments: impl IntoIterator<Item = Moment>) -> Self {
+		Self(moments.into_iter().collect())
+	}
 
-    /// Returns `Self::len() == 0`.
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
+	/// Returns the wrapped `Vec<Moment>`, destroying `self`.
+	pub fn into_inner(self) -> Vec<Moment> {
+		self.0
+	}
 
-    /// Appends another [Sheet] to `self`, destroying the other.
-    ///
-    /// # Remarks
-    /// This method will join two tracks end to end. If you want to merge them
-    /// instead, see [merge_with](Sheet::merge_with).
-    pub fn append(&mut self, other: Self) {
-        self.0.extend(other.0);
-    }
+	/// Returns how many MIDI ticks (or [Moment]s) this [Sheet] has.
+	///
+	/// Note that multiplying this value with the length of a tick may not
+	/// always give you the correct total duration. The reason for this is a
+	/// MIDI file can change tempo mid-track, however it is still trivial to
+	/// calculate the duration since every tempo-change event will be contained
+	/// in `self`.
+	pub fn len(&self) -> usize {
+		self.0.len()
+	}
 
-    /// Merges `self` with another [Sheet], destroying the other.
-    ///
-    /// # Remarks
-    /// This method will combine every moment in both [Sheet]s into one. If you
-    /// want to join them end to end instead, see [Sheet::append].
-    pub fn merge_with(&mut self, other: Self) {
-        if other.len() > self.len() {
-            let n_new = other.len() - self.len();
-            self.0.extend((0..n_new).map(|_| Moment::Empty))
-        }
+	/// Returns `Self::len() == 0`.
+	pub fn is_empty(&self) -> bool {
+		self.0.is_empty()
+	}
 
-        for (i, moment) in other.0.into_iter().enumerate() {
-            if let Moment::Events(events) = moment {
-                for e in events {
-                    self[i].push(e);
-                }
-            }
-        }
-    }
+	/// Appends another [Sheet] to `self`, destroying the other.
+	///
+	/// # Remarks
+	/// This method will join two tracks end to end. If you want to merge them
+	/// instead, see [merge_with](Sheet::merge_with).
+	pub fn append(&mut self, other: Self) {
+		self.0.extend(other.0);
+	}
 
-    /// Returns an iterator over every moment in `self`.
-    pub fn iter(&self) -> std::slice::Iter<'_, Moment> {
-        self.0.iter()
-    }
+	/// Merges `self` with another [Sheet], destroying the other.
+	///
+	/// # Remarks
+	/// This method will combine every moment in both [Sheet]s into one. If you
+	/// want to join them end to end instead, see [Sheet::append].
+	pub fn merge_with(&mut self, other: Self) {
+		if other.len() > self.len() {
+			let n_new = other.len() - self.len();
+			self.0.extend((0..n_new).map(|_| Moment::Empty))
+		}
+
+		for (i, moment) in other.0.into_iter().enumerate() {
+			if let Moment::Events(events) = moment {
+				for e in events {
+					self[i].push(e);
+				}
+			}
+		}
+	}
+
+	/// Returns an iterator over every moment in `self`.
+	pub fn iter(&self) -> std::slice::Iter<'_, Moment> {
+		self.0.iter()
+	}
+
+	/// Add a single [Moment] at the end of this sheet.
+	pub fn push(&mut self, m: Moment) {
+		self.0.push(m);
+	}
 }
--- src/sheet/bar.rs
+++ src/sheet/bar.rs
@@ -1,102 +1,107 @@
 use std::collections::VecDeque;
 
-use crate::{Event, Moment, Sheet};
+use crate::{
+	Event,
+	Moment,
+	Sheet,
+};
 
 #[derive(Eq, PartialEq, Copy, Clone)]
 struct TimeSignature {
-    // Beats per bar.
-    numerator: u8,
-    // Note of a beat. A negative power of 2.
-    denominator: u8,
+	// Beats per bar.
+	numerator: u8,
+	// Note of a beat. A negative power of 2.
+	denominator: u8,
 }
 
 impl TimeSignature {
-    fn bar_32s(&self) -> f32 {
-        let note_as_32s = 2_f32.powi(5_i32 - self.denominator as i32);
-        // let note_as_32s = 32.0 / self.denominator as f32;
-        self.numerator as f32 * note_as_32s
-    }
+	fn bar_32s(&self) -> f32 {
+		let note_as_32s = 2_f32.powi(5_i32 - self.denominator as i32);
+		// let note_as_32s = 32.0 / self.denominator as f32;
+		self.numerator as f32 * note_as_32s
+	}
 }
 
+/// An iterator over bars in a MIDI [Sheet].
 pub struct Bars {
-    time_sig: TimeSignature,
-    // beat_32s: u8,
-    tpb: f32,
-    buf: VecDeque<Moment>,
+	time_sig: TimeSignature,
+	// beat_32s: u8,
+	tpb: f32,
+	buf: VecDeque<Moment>,
 }
 
 impl Iterator for Bars {
-    type Item = Vec<Moment>;
+	type Item = Vec<Moment>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.buf.is_empty() {
-            return None;
-        }
-        // Check if start of bar has time signature.
-        let first = self.buf.pop_front().unwrap();
-        if let Some(time_sig) = find_time_sig(&first) {
-            self.time_sig = time_sig;
-        }
-        if self.buf.is_empty() {
-            return Some(vec![first]);
-        }
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.buf.is_empty() {
+			return None;
+		}
+		// Check if start of bar has time signature.
+		let first = self.buf.pop_front().unwrap();
+		if let Some(time_sig) = find_time_sig(&first) {
+			self.time_sig = time_sig;
+		}
+		if self.buf.is_empty() {
+			return Some(vec![first]);
+		}
 
-        let len_32nd = self.tpb / 8.0; // self.beat_32s as f32;
-        let chunk_len = (self.time_sig.bar_32s() * len_32nd as f32) as usize;
-        let mut temp = Vec::with_capacity(chunk_len);
-        temp.push(first);
+		let len_32nd = self.tpb / 8.0; // self.beat_32s as f32;
+		let chunk_len = (self.time_sig.bar_32s() * len_32nd as f32) as usize;
+		let mut temp = Vec::with_capacity(chunk_len);
+		temp.push(first);
 
-        for _ in 1..chunk_len {
-            if let Some(moment) = self.buf.pop_front() {
-                let time_sig = find_time_sig(&moment);
-                temp.push(moment);
-                if let Some(time_sig) = time_sig {
-                    if time_sig != self.time_sig {
-                        self.time_sig = time_sig;
-                        break;
-                    }
-                }
-            } else {
-                break;
-            }
-        }
+		for _ in 1..chunk_len {
+			if let Some(moment) = self.buf.pop_front() {
+				let time_sig = find_time_sig(&moment);
+				temp.push(moment);
+				if let Some(time_sig) = time_sig {
+					if time_sig != self.time_sig {
+						self.time_sig = time_sig;
+						break;
+					}
+				}
+			} else {
+				break;
+			}
+		}
 
-        Some(temp)
-    }
+		Some(temp)
+	}
 }
 
 impl Sheet {
-    /// Returns an iterator that yields measures (bars) from this sheet.
-    ///
-    /// # Arguments
-    /// - `ticks_per_beat`: Obtained from a [Header](midly::Header), same value
-    ///   used for constructing a [Ticker](crate::Ticker).
-    pub fn into_bars(self, ticks_per_beat: u16) -> Bars {
-        Bars {
-            tpb: ticks_per_beat as f32,
-            time_sig: TimeSignature {
-                numerator: 4,
-                denominator: 4,
-            },
-            // beat_32s: 24,
-            buf: self.0.into(),
-        }
-    }
+	/// Returns an iterator that yields measures (bars) from this sheet.
+	///
+	/// # Arguments
+	/// - `ticks_per_beat`: Obtained from a [Header](midly::Header), same value
+	///   used for constructing a [Ticker](crate::Ticker).
+	pub fn into_bars(self, ticks_per_beat: u16) -> Bars {
+		Bars {
+			tpb: ticks_per_beat as f32,
+			time_sig: TimeSignature {
+				numerator: 4,
+				denominator: 4,
+			},
+			// beat_32s: 24,
+			buf: self.0.into(),
+		}
+	}
 }
 
 fn find_time_sig(m: &Moment) -> Option<TimeSignature> {
-    match m {
-        Moment::Empty => None,
-        Moment::Events(events) => {
-            for e in events {
-                if let Event::TimeSignature(n, d, ..) = e {
-                    return Some(TimeSignature {
-                        numerator: *n,
-                        denominator: *d,
-                    });
-                }
-            }
-            None
-        }
-    }
+	match m {
+		Moment::Empty => None,
+		Moment::Events(events) => {
+			for e in events {
+				if let Event::TimeSignature(n, d, ..) = e {
+					return Some(TimeSignature {
+						numerator: *n,
+						denominator: *d,
+					});
+				}
+			}
+			None
+		}
+	}
 }

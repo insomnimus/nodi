a06d2b2583c9871e347893431185f1947d6bb389
Taylan Gökkaya <insomnimus@protonmail.com>
2022-06-07 01:59:07 +0300

    revert: removed the Transpose composer

--- src/compose.rs
+++ src/compose.rs
@@ -1,24 +1,11 @@
 //! Contains various small types that implement [Connection] that add extra capabilities to another [Connection] by wrapping them.
 
 use crate::{Connection, MidiEvent};
-use std::sync::{
-	atomic::{AtomicI8, Ordering},
-	Arc,
-};
 
 /// [Connection] combinators.
 ///
 /// This trait is implemented for all types that implement [Connection].
 pub trait Compose: Connection + Sized {
-	/// Returns a [Connection] that transposes its input and calls `self` to play it.
-	fn transpose(self, shift: i8, transpose_ch9: bool) -> Transpose<Self> {
-		Transpose {
-			con: self,
-			transpose_ch9,
-			shift: Arc::new(AtomicI8::new(shift)),
-		}
-	}
-
 	/// Returns a [Connection] that maps its input and calls `self` to play it.
 	fn map<F: FnMut(MidiEvent) -> MidiEvent>(self, f: F) -> Map<Self, F> {
 		Map { con: self, f }
@@ -37,27 +24,6 @@ pub trait Compose: Connection + Sized {
 }
 
 impl<C: Connection> Compose for C {}
-
-/// A transposing [Connection], Created by calling [Compose::transpose] on an existing connection.
-#[derive(Clone, Debug)]
-pub struct Transpose<C: Connection> {
-	/// The transposition in semi-tones.
-	pub shift: Arc<AtomicI8>,
-	/// If set to `true`, the drum channel (ch 9) will also be transposed.
-	pub transpose_ch9: bool,
-	/// The wrapped [Connection].
-	pub con: C,
-}
-
-impl<C: Connection> Connection for Transpose<C> {
-	#[inline]
-	fn play(&mut self, event: MidiEvent) -> bool {
-		match event.transposed(self.shift.load(Ordering::SeqCst), self.transpose_ch9) {
-			Some(e) => self.con.play(e),
-			None => true,
-		}
-	}
-}
 
 /// A mapping [Connection]. Created by calling [Compose::map] on an existing connection.
 #[derive(Clone, Debug)]
